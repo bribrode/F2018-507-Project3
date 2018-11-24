5d14fcc6cc230002e6b8ae8a2294750172e07879
Brianna Broderick <bribrode@umich.edu>
2018-11-24 16:07:51 -0500

    Fills Bars table with data

--- proj3_choc.py
+++ proj3_choc.py
@@ -63,7 +63,7 @@ def create_db():
 
 ##Checks to see if value is in JSON Database, if it is not, returns NULL
 ##Simply to avoid writing out repeated checks
-def check_data(dictName, keyName):
+def check_data_json(dictName, keyName):
     if not dictName[keyName]:
         return None
     else:
@@ -73,35 +73,119 @@ def check_data(dictName, keyName):
 def populate_countries(country_json):
     conn = sqlite3.connect(DBNAME)
     cur = conn.cursor()
-    file = open(country_json)
-    countryInfo = json.load(file)
 
-    for country in countryInfo:
-        engName = check_data(country, "name")
-        alpha2 = check_data(country, "alpha2Code")
-        alpha3 = check_data(country, "alpha3Code")
-        region = check_data(country, "region")
-        subregion = check_data(country, "subregion")
-        pop = check_data(country, "population")
-        area = check_data(country, "area")
+    with open(country_json) as file:
+        countryInfo = json.load(file)
 
-        vals = (None, alpha2, alpha3, engName, region, subregion, pop, area)
+        for country in countryInfo:
+            engName = check_data_json(country, "name")
+            alpha2 = check_data_json(country, "alpha2Code")
+            alpha3 = check_data_json(country, "alpha3Code")
+            region = check_data_json(country, "region")
+            subregion = check_data_json(country, "subregion")
+            pop = check_data_json(country, "population")
+            area = check_data_json(country, "area")
 
-        statement = 'INSERT INTO "Countries" VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
-        cur.execute(statement, vals)
+            vals = (None, alpha2, alpha3, engName, region, subregion, pop, area)
+
+            statement = 'INSERT INTO "Countries" VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+            cur.execute(statement, vals)
 
     conn.commit()
     conn.close()
 
 
+##Clean handling of csv data
+##Returns value as None if not in csv
+##Strips % from percentage data
+##Pulls foreign keys for Countries
+def check_data_csv(rowList, index):
+    if not rowList[index]:
+        return None
+    elif index == 4:
+        return rowList[index][:-1]
+    elif index == 5 or index == 8:
+        if rowList[index] == "Unknown":
+            return None
+        else:
+            conn = sqlite3.connect(DBNAME)
+            cur = conn.cursor()
+
+            statement = 'SELECT Id FROM "Countries" WHERE EnglishName = ?'
+            val = (rowList[index],)
+            cur.execute(statement, val)
+
+            for row in cur:
+                return row[0]
+
+            conn.close()
+    else:
+        return rowList[index]
+
+
+
 ##Read info from country JSON and appropriately insert into table
 def populate_bars(bar_csv):
-    pass
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+
+    with open(bar_csv) as file:
+        csvReader = csv.reader(file)
+        ##Skip headers
+        next(csvReader)
+        for row in csvReader:
+            company = check_data_csv(row, 0)
+            beanbarName = check_data_csv(row, 1)
+            ref = check_data_csv(row, 2)
+            reviewDate = check_data_csv(row, 3)
+            cocoapercent = check_data_csv(row, 4)
+            companyLocId = check_data_csv(row, 5)
+            rating = check_data_csv(row, 6)
+            beanType = check_data_csv(row, 7)
+            broadBeanId = check_data_csv(row, 8)
+
+            vals = (None, company, beanbarName, ref, reviewDate, cocoapercent, companyLocId, rating, beanType, broadBeanId)
+
+            statement = 'INSERT INTO "Bars" VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+            cur.execute(statement, vals)
+
+    conn.commit()
+    conn.close()
+
 
 
 # Part 2: Implement logic to process user commands
 def process_command(command):
-    return []
+    queryList = []
+    commandList = command.split()
+    if commandList[0].lower() == "bars":
+        if len(commandList) > 1:
+            print("here!")
+            for x in range(1,len(commandList)-1):
+                if commandList[x][1:11].lower() == "sellcountry":
+                    print("sell country")
+                elif commandList[x][0:13].lower() == "sourcecountry":
+                    print("sourcecountry")
+                elif commandList[x][0:10].lower() == "sellregion":
+                    print("sellregion")
+                elif commandList[x][0:12].lower() == "sourceregion":
+                    print("sourceregion")
+
+
+            ###sellcountry=alpha2, sourcecountry=alpha2, sellregion=name, sourceregion=name (default none)
+            ###ratings | cocoa (default ratings)
+            ###top=limit | bottom=limit (default = 10)
+        print("bars!")
+    elif commandList[0].lower() == "companies":
+        print("companies!")
+    elif commandList[0].lower() == "countries":
+        print("countries!")
+    elif commandList[0].lower() == "regions":
+        print("regions!")
+    else:
+        print("Please enter a valid command")
+
+    return queryList
 
 
 def load_help_text():
@@ -118,9 +202,12 @@ def interactive_prompt():
         if response == 'help':
             print(help_text)
             continue
+        else:
+            process_command(response)
 
 # Make sure nothing runs or prints out when this file is run as a module
 if __name__=="__main__":
     create_db()
     populate_countries(COUNTRIESJSON)
-    # interactive_prompt()
+    populate_bars(BARSCSV)
+    interactive_prompt()
