0c23d8c9297b970abccf49db6adae25c64561f91
Brianna Broderick <bribrode@umich.edu>
2018-11-24 13:58:26 -0500

    Creates Database & fills with Country Info

--- proj3_choc.py
+++ proj3_choc.py
@@ -1,3 +1,7 @@
+##Brianna Broderick
+##SI 507 - Fall 2018
+##Project 3
+
 import sqlite3
 import csv
 import json
@@ -7,10 +11,92 @@ import json
 # and meet the project requirements! You will need to implement several new
 # functions.
 
-# Part 1: Read data from CSV and JSON into a new database called choc.db
+# Part 1: Read data frxom CSV and JSON into a new database called choc.db
 DBNAME = 'choc.db'
 BARSCSV = 'flavors_of_cacao_cleaned.csv'
 COUNTRIESJSON = 'countries.json'
+
+##Wipes tables from database if they exist and (re)builds the tables
+def create_db():
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+
+    ##WIPE THE DB
+    statement = "DROP TABLE IF EXISTS 'Bars'"
+    cur.execute(statement)
+
+    statement = "DROP TABLE IF EXISTS 'Countries'"
+    cur.execute(statement)
+
+    ##Build the Bars Table
+    statement = '''
+        CREATE TABLE 'Bars' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Company' TEXT,
+            'SpecificBeanBarName' TEXT,
+            'REF' TEXT,
+            'ReviewDate' TEXT,
+            'CocoaPercent' REAL,
+            'CompanyLocationId' INTEGER,
+            'Rating' REAL,
+            'BeanType'  TEXT,
+            'BroadBeanOriginId' INTEGER )
+        '''
+    cur.execute(statement)
+
+    ##Build the Countries Table
+    statement = '''
+        CREATE TABLE 'Countries' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Alpha2' TEXT,
+            'Alpha3' TEXT,
+            'EnglishName' TEXT,
+            'Region' TEXT,
+            'Subregion' TEXT,
+            'Population' INTEGER,
+            'Area' REAL )
+        '''
+    cur.execute(statement)
+
+    conn.commit()
+    conn.close()
+
+##Checks to see if value is in JSON Database, if it is not, returns NULL
+##Simply to avoid writing out repeated checks
+def check_data(dictName, keyName):
+    if not dictName[keyName]:
+        return None
+    else:
+        return dictName[keyName]
+
+##Read info from country JSON and appropriately insert into table
+def populate_countries(country_json):
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+    file = open(country_json)
+    countryInfo = json.load(file)
+
+    for country in countryInfo:
+        engName = check_data(country, "name")
+        alpha2 = check_data(country, "alpha2Code")
+        alpha3 = check_data(country, "alpha3Code")
+        region = check_data(country, "region")
+        subregion = check_data(country, "subregion")
+        pop = check_data(country, "population")
+        area = check_data(country, "area")
+
+        vals = (None, alpha2, alpha3, engName, region, subregion, pop, area)
+
+        statement = 'INSERT INTO "Countries" VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+        cur.execute(statement, vals)
+
+    conn.commit()
+    conn.close()
+
+
+##Read info from country JSON and appropriately insert into table
+def populate_bars(bar_csv):
+    pass
 
 
 # Part 2: Implement logic to process user commands
@@ -35,4 +121,6 @@ def interactive_prompt():
 
 # Make sure nothing runs or prints out when this file is run as a module
 if __name__=="__main__":
-    interactive_prompt()
+    create_db()
+    populate_countries(COUNTRIESJSON)
+    # interactive_prompt()
